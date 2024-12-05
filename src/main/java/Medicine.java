98e8963954a87f0821db6aa994885a4298133647
longvupham <pplvu144@gmail.com>
2024-12-05 18:40:07 +0700

    update0512

--- src/main/java/Medicine.java
+++ src/main/java/Medicine.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.xpath.internal.objects.XString;
-
 public class Medicine {
     private int id;
     private String name;
@@ -19,41 +17,19 @@ public class Medicine {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getManufacturer() {
         return manufacturer;
     }
 
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
-
     public double getPrice() {
         return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
     }
 
     public int getStock() {
         return stock;
     }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
-
-
---
+++ src/main/java/org/example/democ0624/MedicineServlet.java
@@ -0,0 +1,43 @@
+package org.example.democ0624;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "MedicineServlet", urlPatterns = "/medicines")
+public class MedicineServlet extends HttpServlet {
+    private MedicineModel medicineModel;
+
+    @Override
+    public void init() throws ServletException {
+        medicineModel = new MedicineModel();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        List<Medicine> medicines = medicineModel.getAllMedicines();
+
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Danh sách thuốc</h1>");
+        out.println("<table border='1'>");
+        out.println("<tr><th>ID</th><th>Name</th><th>Manufacturer</th><th>Price</th><th>Stock</th></tr>");
+
+        for (Medicine medicine : medicines) {
+            out.println("<tr>");
+            out.println("<td>" + medicine.getId() + "</td>");
+            out.println("<td>" + medicine.getName() + "</td>");
+            out.println("<td>" + medicine.getManufacturer() + "</td>");
+            out.println("<td>" + medicine.getPrice() + "</td>");
+            out.println("<td>" + medicine.getStock() + "</td>");
+            out.println("</tr>");
+        }
+        out.println("</table>");
+        out.println("</body></html>");
+    }
+}
